4fc725e94127ed186a4d1c7ec571e8d24a9d57cc
Davit Kalantaryan <davit.kalantaryan@desy.de>
2023-12-07 19:41:32 +0100

    Feature/1 setup socket leak investigator repository (#2)
    * added necessary submodule for investigation
    * improved .gitignore
    * switching to the last commit of the contrib/programs_investigator submodule
    * added .gitattributes to have repo specific attributes
    * added test project to test socket leak handlers
    * switching to the last commit of the master branch in the submodule contrib/programs_investigator

---
+++ src/tests/main_socket_keak_analyze_test01.cpp
@@ -0,0 +1,18 @@
+//
+// repo:			socket_leak_investigator
+// file:            main_socket_keak_analyze_test01.cpp
+// path:			src/tests/main_socket_keak_analyze_test01.cpp
+// created on:		2023 Dec 07
+// created by:		Davit Kalantaryan (davit.kalantaryan@desy.de)
+//
+
+
+#include <cinternal/internal_header.h>
+
+int main(int a_argc, char* a_argv[])
+{
+	(void)a_argc;
+	(void)a_argv;
+
+	return 0;
+}
