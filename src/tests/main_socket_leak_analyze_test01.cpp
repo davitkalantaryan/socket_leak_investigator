13d781477cb70358d8be25acfba8c3c16cdff117
Davit Kalantaryan <davit.kalantar@desy.de>
2023-12-11 17:18:57 +0100

    simplified main of test executable

--- src/tests/main_socket_leak_analyze_test01.cpp
+++ src/tests/main_socket_leak_analyze_test01.cpp
@@ -15,6 +15,8 @@
 #include <WS2tcpip.h>
 #include <Windows.h>
 typedef SOCKET  socket_t;
+#define SleepMsIntr(_x)     SleepEx(_x,TRUE)
+#define getpid              GetCurrentProcessId
 #endif
 #include <cinternal/undisable_compiler_warnings.h>
 
@@ -25,138 +27,51 @@ int main(int a_argc, char* a_argv[])
 	(void)a_argc;
 	(void)a_argv;
 
-	{
-        WORD wVersionRequested;
-        WSADATA wsaData;
-        //SOCKET aTestSocket;
-
-        wVersionRequested = MAKEWORD(2, 2);
-
-        if (WSAStartup(wVersionRequested, &wsaData) != 0) {
-            exit(1);
-        }
-
-        /* Confirm that the WinSock DLL supports 2.2.		*/
-        /* Note that if the DLL supports versions greater	*/
-        /* than 2.2 in addition to 2.2, it will still return*/
-        /* 2.2 in wVersion since that is the version we		*/
-        /* requested.										*/
-
-        if (LOBYTE(wsaData.wVersion) != 2 || HIBYTE(wsaData.wVersion) != 2) {
-            WSACleanup();
-            exit(1);
-        }
-
-        //aTestSocket = socket(AF_INET, SOCK_STREAM, 0);
-        //if (aTestSocket == INVALID_SOCKET) {
-        //    exit(1);
-        //}
-        //closesocket(aTestSocket);
-	}
+    int pid = (int)getpid();
 
 	while (1) {
 		aSocket = socket(AF_INET, SOCK_STREAM, 0);
-		fprintf(stdout, "aSocket = %d\n", (int)aSocket);
+		fprintf(stdout, "pid: %d, aSocket = %d\n", pid, (int)aSocket);
+        SleepMsIntr(2000);
 	}
-
-
-    {
-        WSACleanup();
-    }
-
 
 	return 0;
 }
 
 
-//#pragma pack(1)
-//struct patch_t
-//{
-//	BYTE nPatchType; //OP code, 0xE9 for JMP
-//	DWORD dwAddress;
-//};
-//#pragma pack()
-//
-//
-//BOOL apply_patch(BYTE eType, DWORD dwAddress, const void* pTarget, DWORD* a_orig_size, BYTE* replaced)
-//{
-//	SIZE_T orig_size;
-//	DWORD dwOldValue, dwTemp;
-//	struct patch_t pWrite =
-//	{
-//		eType,
-//		(DWORD)pTarget - (dwAddress + sizeof(DWORD) + sizeof(BYTE))
-//	};
-//	VirtualProtect((LPVOID)dwAddress, sizeof(DWORD), PAGE_EXECUTE_READWRITE, &dwOldValue);
-//	ReadProcessMemory(GetCurrentProcess(), (LPVOID)dwAddress, (LPVOID)replaced, sizeof(pWrite), &orig_size); //Keep track of the bytes we replaced
-//	BOOL bSuccess = WriteProcessMemory(GetCurrentProcess(), (LPVOID)dwAddress, &pWrite, sizeof(pWrite), NULL);
-//	VirtualProtect((LPVOID)dwAddress, sizeof(DWORD), dwOldValue, &dwTemp);
-//
-//	*a_orig_size = orig_size;
-//
-//	return bSuccess;
-//}
-//
-//
-//static DWORD addr_socket = 0;
-//static DWORD addr_closesocket = 0;
-//static DWORD orig_size_socket = 0;
-//static DWORD orig_size_closesocket = 0;
-//static BYTE replaced_socket[10];
-//static BYTE replaced_closesocket[10];
-//typedef SOCKET(WSAAPI* TypeSocket)(int af, int type, int protocol);
-//typedef int (*TypeClosesocket)(SOCKET s);
-//static TypeSocket s_socket_original;
-//static TypeClosesocket s_closesocket_original;
-//static TypeSocket s_pSocket = NULL;
-//static TypeClosesocket s_pClosesocket = NULL;
-//
-//static SOCKET WSAAPI SockLeakHookCrtSocketStatic(int a_af, int a_type, int a_protocol) CPPUTILS_NOEXCEPT
-//{
-//	return (*s_socket_original)(a_af, a_type, a_protocol);
-//}
-//
-//
-//static int SockLeakHookRemSocketStatic(SOCKET a_sock) CPPUTILS_NOEXCEPT
-//{
-//	return (*s_closesocket_original)(a_sock);
-//}
-//
-//
-//static DWORD WINAPI initialize(void)
-//{
-//	DWORD addr;
-//	BYTE replaced[10];
-//	DWORD orig_size;
-//
-//
-//	addr_socket = (DWORD)GetProcAddress(GetModuleHandle(TEXT("WS2_32.dll")), "socket");
-//	addr_closesocket = (DWORD)GetProcAddress(GetModuleHandle(TEXT("WS2_32.dll")), "closesocket");
-//
-//	s_socket_original = (TypeSocket)GetProcAddress(GetModuleHandle(TEXT("WS2_32.dll")), "socket");
-//	s_closesocket_original = (TypeClosesocket)GetProcAddress(GetModuleHandle(TEXT("WS2_32.dll")), "closesocket");
-//
-//	//TODO: Clean this area up and move these to some inline function
-//	addr = addr_socket;
-//	if (apply_patch(0xE9, addr, (void*)(&SockLeakHookCrtSocketStatic), &orig_size_socket, replaced_socket)) //Note we only store this replaced because this is the original winsock function code, which we need to put back upon closing
-//	{
-//		s_pSocket = (TypeSocket)VirtualAlloc(NULL, orig_size_socket << 2, MEM_RESERVE | MEM_COMMIT, PAGE_EXECUTE_READWRITE);
-//		memcpy((void*)s_pSocket, replaced_socket, orig_size_socket);
-//		apply_patch(0xE9, (DWORD)s_pSocket + orig_size_socket, (void*)(addr + orig_size_socket), &orig_size, replaced);
-//	}
-//
-//	addr = addr_closesocket;
-//	if (apply_patch(0xE9, addr, (void*)(&SockLeakHookRemSocketStatic), &orig_size_closesocket, replaced_closesocket))
-//	{
-//		s_pClosesocket = (TypeClosesocket)VirtualAlloc(NULL, orig_size_closesocket << 2, MEM_RESERVE | MEM_COMMIT, PAGE_EXECUTE_READWRITE);
-//		memcpy((void*)s_pClosesocket, replaced_closesocket, orig_size_closesocket);
-//		apply_patch(0xE9, (DWORD)s_pClosesocket + orig_size_closesocket, (void*)(addr + orig_size_closesocket), &orig_size, replaced);
-//	}
-//
-//	//Initialize lists
-//	//INIT_LIST_HEAD(&ws_handlers.ws_handlers_send);
-//	//INIT_LIST_HEAD(&ws_handlers.ws_handlers_recv);
-//	//INIT_LIST_HEAD(&ws_plugins.plugins);
-//	//load_plugins("./plugins/", &ws_plugins);
-//	return 0;
-//}
+
+
+
+static void main_socket_leak_analyze_test01_c_clean(void) CPPUTILS_NOEXCEPT {
+#ifdef _WIN32
+    WSACleanup();
+#endif
+}
+
+
+CPPUTILS_CODE_INITIALIZER(main_socket_leak_analyze_test01_c_init) {
+#ifdef _WIN32
+    WORD wVersionRequested;
+    WSADATA wsaData;
+    //SOCKET aTestSocket;
+
+    wVersionRequested = MAKEWORD(2, 2);
+
+    if (WSAStartup(wVersionRequested, &wsaData) != 0) {
+        exit(1);
+    }
+
+    /* Confirm that the WinSock DLL supports 2.2.		*/
+    /* Note that if the DLL supports versions greater	*/
+    /* than 2.2 in addition to 2.2, it will still return*/
+    /* 2.2 in wVersion since that is the version we		*/
+    /* requested.										*/
+
+    if (LOBYTE(wsaData.wVersion) != 2 || HIBYTE(wsaData.wVersion) != 2) {
+        WSACleanup();
+        exit(1);
+    }
+
+    atexit(&main_socket_leak_analyze_test01_c_clean);
+#endif
+}
